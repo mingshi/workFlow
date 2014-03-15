e45f615b43169eedd8ccb96dab2dca4e15923f98
mingshi <fivemingshi@gmail.com>
2014-03-15 16:37:26 +0800

    .

--- wf/views/flow.py
+++ wf/views/flow.py
@@ -5,7 +5,7 @@ Created :   2014/03/12 16:13:39
 FileName:   flow.py
 '''
 
-from flask import Blueprint, render_template, request, url_for, redirect, session, g
+from flask import Blueprint, render_template, request, url_for, redirect, session, g, jsonify
 from wf import app
 from wf.db.db import db_session
 from wf.model import *
@@ -18,6 +18,8 @@ import math
 from wf.util.libs import *
 from datetime import date
 from formValidate.testFlowForm import testFlowForm
+import os
+import time
 
 mod = Blueprint('flow', __name__)
 
@@ -47,3 +49,47 @@ def flow_add_by_type(id) :
     else :
         return render_template('wf/' + temp, today = today, form = form)
 
+@mod.route('/flow/uploadFile', methods=['POST'])
+def uploadFile() :
+    if request.method == 'POST' :
+        data_file = request.files.get('files[]')
+        file_name = str(int(time.time())) + "_" + data_file.filename
+        
+        if not os.path.exists(app.config['UPLOAD_PATH']) :
+            os.makedirs(app.config['UPLOAD_PATH'])
+
+        save_path = app.config['UPLOAD_PATH'] + '/' + file_name
+        data_file.save(save_path)
+        
+        file_url = url_for('static', filename = 'upload/' + file_name)
+        
+        file_size = os.path.getsize(app.config['ROOT_PATH'] + '/wf' + file_url)
+
+        one = {}
+
+        one['name'] = file_name
+        one['size'] = file_size
+        one['url'] = file_url
+        one['thumbnailUrl'] = file_url
+        one['deleteUrl'] = "/flow/deleteUpload/?" + "file=" + file_name + "&_method=DELETE"
+        one['deleteType'] = "POST"
+
+        result = {}
+        result['files'] = []
+
+        result['files'].append(one)
+
+        return json.dumps(result)
+
+@mod.route('/flow/deleteUpload/', methods=['POST'])
+def deleteUpload() :
+    file_name = request.args.get('file')
+    file_url = url_for('static', filename = 'upload/' + file_name)
+    absolute_url = app.config['ROOT_PATH'] + '/wf' + file_url 
+    
+    if os.path.isfile(absolute_url) :
+        os.remove(absolute_url)
+
+    result = {}
+    result[file_name] = True
+    return json.dumps(result)
